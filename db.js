a97122b94838298b250090c9080cadfef3bcd53b
Chell <im.chell@outlook.com>
2023-04-26 22:12:18 +0200

    feat: add cereal knowledge base init

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const { Client } = require("pg");
+const fs = require("fs");
 
 function dbInit() {
   const client = new Client({ connectionString: process.env.DATABASE_URL });
@@ -24,6 +25,57 @@ function dbInit() {
         }
       );
     }
+  });
+
+  cerealKnowledgeBaseInit(client);
+
+  return client;
+}
+
+function cerealKnowledgeBaseInit(client) {
+  const initQuery = `
+  CREATE TABLE cerealKnowledgeBase (
+    Crop TEXT,
+    Min_Planting_Temperature INTEGER,
+    Max_Planting_Temperature INTEGER,
+    Planting_Start_Month INTEGER,
+    Planting_End_Month INTEGER,
+    Description TEXT
+  );
+`;
+  client.query(initQuery, (err, res) => {
+    if (err) {
+      console.error(err);
+      client.end();
+      return;
+    }
+
+    console.log("Table created successfully");
+
+    // Import data from CSV file
+    const csvFilePath = "./data/cereal.csv";
+    const csvData = fs.readFileSync(csvFilePath, "utf8");
+
+    const importQuery = `
+    COPY cerealKnowledgeBase (Crop, Min_Planting_Temperature, Max_Planting_Temperature, Planting_Start_Month, Planting_End_Month, Description) FROM STDIN WITH (FORMAT CSV, HEADER true)
+  `;
+
+    const stream = client.query(copyFrom(importQuery));
+    stream.on("error", (err) => {
+      console.error(err);
+      client.end();
+      return;
+    });
+
+    stream.on("end", () => {
+      console.log("Data imported successfully");
+      client.end();
+    });
+
+    const csvStream = new Readable();
+    csvStream.push(csvData);
+    csvStream.push(null);
+    csvStream.pipe(stream);
   });
 }
 
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const dbInit = require("./db.js");
 const app = express();
 const port = process.env.PORT || 3000;
 
-dbInit();
+const client = dbInit();
 
 app.get(["/"], (req, res) => {
   greeting = "<h1>Hello From Group 6!</h1>";
