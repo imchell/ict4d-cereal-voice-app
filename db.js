0d39f972dc75f3651f4eb8048921180b950da938
Chell <im.chell@outlook.com>
2023-04-26 23:15:04 +0200

    feat: force init database

--- db.js
+++ db.js
@@ -33,7 +33,8 @@ function dbInit() {
 }
 
 function cerealKnowledgeBaseInit(client) {
-  const initQuery = `CREATE TABLE IF NOT EXISTS cerealKnowledgeBase (
+  const initQuery = `DROP TABLE IF EXISTS cerealKnowledgeBase;
+  CREATE TABLE IF NOT EXISTS cerealKnowledgeBase (
     Crop TEXT,
     Min_Planting_Temperature INTEGER,
     Max_Planting_Temperature INTEGER,
@@ -45,7 +46,18 @@ function cerealKnowledgeBaseInit(client) {
     SELECT 'Rice', '20', '35', '6', '9', 'Rice is one of the main staple crops in Mali, typically planted during the rainy season. It requires ample water supply and is primarily grown in the Niger River Valley and inland rice cultivation areas.'
     WHERE NOT EXISTS (
       SELECT Crop FROM cerealKnowledgeBase WHERE Crop = 'Rice'
-    );`;
+    );
+    INSERT INTO cerealKnowledgeBase(Crop, Min_Planting_Temperature, Max_Planting_Temperature, Planting_Start_Month, Planting_End_Month, Description)
+    SELECT 'Cotton', '15', '34', '5', '9', 'Cotton is one of the main cash crops in Mali, contributing significantly to the country's export revenue. Cotton cultivation is mainly concentrated in the southern regions, such as Sikasso, Kayes, and Koulikoro.'
+    WHERE NOT EXISTS (
+      SELECT Crop FROM cerealKnowledgeBase WHERE Crop = 'Cotton'
+    );
+    INSERT INTO cerealKnowledgeBase(Crop, Min_Planting_Temperature, Max_Planting_Temperature, Planting_Start_Month, Planting_End_Month, Description)
+    SELECT 'Sorghum', '21', '32', '5', '9', 'Sorghum is one of the main staple crops in Mali, characterized by its drought tolerance and wide adaptability. In Mali, sorghum is mainly used for human consumption and animal feed, and is extensively cultivated throughout the country.'
+    WHERE NOT EXISTS (
+      SELECT Crop FROM cerealKnowledgeBase WHERE Crop = 'Sorghum'
+    );
+    `;
   client.query(initQuery, (err, res) => {
     if (err) {
       console.error(err);
